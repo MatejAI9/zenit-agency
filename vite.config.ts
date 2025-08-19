32f4927e77801128efad4ffb8d2a400062bd86ce
MatejAI9 <mateja.lukic7@gmail.com>
2025-08-19 20:11:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/zenit-agency/',   // <— bitno
+  base: './',           // ← ovo mora
   plugins: [react()],
 })
